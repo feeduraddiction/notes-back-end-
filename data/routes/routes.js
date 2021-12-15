aca705aef036a3678760f082cfe985df1773fe4b
feeduraddiction <alfanex62@gmail.com>
2021-12-16 00:40:42 +0300

    clearance

--- data/routes/routes.js
+++ data/routes/routes.js
@@ -1,15 +1,10 @@
-/* eslint-disable linebreak-style */
-// load up our shiny new route for users
 const userRoutes = require('../notes');
 
 console.log(userRoutes, 'notesRoutes');
 const appRouter = (app, fs) => {
-  // we've added in a default route here that handles empty routes
-  // at the base API url
   app.get('/', (req, res) => {
     res.json({ server: 'server' });
   });
-  // run our user route module here to complete the wire up
   userRoutes(app, fs);
 };
 module.exports = appRouter;
